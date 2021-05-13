d6684b179e19b9ecf627f5e89a67a25c03188d2f
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-13 15:42:24 +0545

    add django debug toolbar

--- root/settings/development.py
+++ root/settings/development.py
@@ -1,5 +1,12 @@
 from .base import *
 
 DEBUG = True
-
 ALLOWED_HOSTS = []
+
+INSTALLED_APPS += ['debug_toolbar']
+
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
