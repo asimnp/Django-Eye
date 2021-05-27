52cea6b56b157460430238dbcf0aa093a22a1815
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-27 09:50:14 +0545

    Setup static directory for deploying static contents

--- root/settings/base.py
+++ root/settings/base.py
@@ -99,6 +99,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static/'
 ]
+STATIC_ROOT = BASE_DIR / 'static_production/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
