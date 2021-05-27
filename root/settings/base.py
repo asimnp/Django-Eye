2d05c30d68d801cb35e77157d3f54fee90953616
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-27 09:35:06 +0545

    Setup global templates directory

--- root/settings/base.py
+++ root/settings/base.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'root.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
