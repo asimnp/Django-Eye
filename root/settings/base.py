78b89e667dd367370ac92b94fa1cd5d27f0d909c
Asim Shrestha <asimshrestha608@gmail.com>
2021-06-08 20:50:09 +0545

    Create accounts app & setup it

--- root/settings/base.py
+++ root/settings/base.py
@@ -33,6 +33,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local
+    'apps.accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
